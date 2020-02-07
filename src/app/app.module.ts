b1e517c56b2e00843d64788108d9c242b55ef290
Ashutosh Kumar Akhouri <aka@Ashutoshs-MacBook-Air.local>
2020-02-07 20:49:45 +0530

    10-stars imdb rating using ngx-stars npm pkg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import { RatingModule } from 'ng-starrating';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxStarsModule } from 'ngx-stars';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +20,7 @@ import { CartComponent } from './cart/cart.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RatingModule
+    NgxStarsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
