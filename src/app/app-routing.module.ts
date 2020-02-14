14fef0b8ef96d72d39bec43d66a031db32940cc1
ashutosh_k_akhouri <ashutosh.akhouri@gmail.com>
2020-02-14 12:55:25 +0530

    Tweet Added-Like Working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NavComponent } from './nav/nav.component';
 import { CartComponent } from './cart/cart.component';
+import { TweetComponent } from './tweet/tweet.component';
 
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'cart', component:CartComponent},
+  {path:'tweet', component:TweetComponent},
   {path:'**', redirectTo:''}
 ];
 
